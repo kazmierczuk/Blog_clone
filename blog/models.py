2d7406cf01cf65d74c1e7a116f5c5ea2ffca436c
bej <kkrzysztof2107@gmail.com>
2020-05-05 07:06:40 +0200

    setup models and forms

---
+++ blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    create_date = models.DateTimeFied()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    # go to view of this post
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=32)
+    text = models.TextField()
+    create_date = models.DateTimeFied(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    #go back to post list
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
